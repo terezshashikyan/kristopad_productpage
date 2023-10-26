8a3d739978d8bf288b8fdd629a75a7692bf8c50d
bella-shell <bella.shelllogix@gmail.com>
2023-10-26 19:03:31 +0400

    changes

--- src/Pages/ProductPage/Gallery/index.jsx
+++ src/Pages/ProductPage/Gallery/index.jsx
@@ -1,4 +1,5 @@
 import './style.scss';
+import {useState, react} from 'react';
 import bgImg from '../../../Assets/Images/bgImg.png';
 import img1 from '../../../Assets/Images/1.png';
 import img2 from '../../../Assets/Images/2.png';
@@ -9,19 +10,20 @@ import img6 from '../../../Assets/Images/6.png';
 
 
 function Gallery() {
-    const [mainImg, setMainImg] = useState()
+    const [mainImg, setMainImg] = useState(bgImg);
+    function handleMainImgChange(event) {
+        setMainImg(event.target.src)
+    }
     return (
         <div className="gallery">
-        <div className="gallery__mainImg">
-        <img src= {bgImg} alt="carImg"/>
-        </div>
+        <div className="gallery__mainImg" style={{backgroundImage: `url(${mainImg})`, width: '613.527px', height: '561.664px', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}></div>
         <div className="gallery__images_container">
-        <img src= {img1} alt="carImg"/>
-        <img src= {img2} alt="carImg"/>
-        <img src= {img3} alt="carImg"/>
-        <img src= {img4} alt="carImg"/>
-        <img src= {img5} alt="carImg"/>
-        <img src= {img6} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img1} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img2} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img3} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img4} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img5} alt="carImg"/>
+        <img onClick={handleMainImgChange} src= {img6} alt="carImg"/>
         </div>
         </div>
     )
