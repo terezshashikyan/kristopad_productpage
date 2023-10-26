560d5326d0603b6fd6814bd851632b5cde76c4ca
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-10-27 01:24:34 +0400

    gallery auto change

--- src/Pages/ProductPage/Gallery/index.jsx
+++ src/Pages/ProductPage/Gallery/index.jsx
@@ -1,5 +1,5 @@
 import './style.scss';
-import {useState, react} from 'react';
+import {useState, useEffect, react} from 'react';
 import bgImg from '../../../Assets/Images/bgImg.png';
 import img1 from '../../../Assets/Images/1.png';
 import img2 from '../../../Assets/Images/2.png';
@@ -12,26 +12,50 @@ import img6 from '../../../Assets/Images/6.png';
 function Gallery() {
     const [mainImg, setMainImg] = useState(bgImg);
     const [isZoomed, setIsZoomed] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
+    const imageUrls = [img1, img2, img3, img4, img5, img6];
+    
     function handleMainImgChange(event) {
-        setMainImg(event.target.src)
+        // getting the background image of the clicked div and setting it as the background of the main. 
+        const target = event.target;
+        const computedStyle = window.getComputedStyle(target);
+        const backgroundImage = computedStyle.getPropertyValue('background-image');
+        setMainImg(backgroundImage.match(/url\("(.+)"\)/)[1])
     }
 
     const handleZoomClick = () => {
         setIsZoomed(!isZoomed);
       };
 
+      useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((currentIndex + 1) % imageUrls.length)
+          setMainImg(imageUrls[currentIndex]);
+        }, 3000); 
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [currentIndex]);
+
+      
+
     return (
         <div className="gallery">
-        <div className={`gallery__mainImg ${isZoomed ? 'zoomed-in' : ''}`}  style={{backgroundImage: `url(${mainImg})`}} onClick={handleZoomClick}></div>
-        <div className="gallery__images_container">
-        <img onClick={handleMainImgChange} src= {img1} alt="carImg"/>
-        <img onClick={handleMainImgChange} src= {img2} alt="carImg"/>
-        <img onClick={handleMainImgChange} src= {img3} alt="carImg"/>
-        <img onClick={handleMainImgChange} src= {img4} alt="carImg"/>
-        <img onClick={handleMainImgChange} src= {img5} alt="carImg"/>
-        <img onClick={handleMainImgChange} src= {img6} alt="carImg"/>
-        </div>
+            <div className={`gallery__mainImg ${isZoomed ? 'zoomed-in' : ''}`}  style={{backgroundImage: `url(${mainImg})`}} onClick={handleZoomClick}></div>
+            <div className="gallery__images">
+                {imageUrls.map((imageUrl, index) => {
+                    return (
+                        <div
+                        onClick = {handleMainImgChange}
+                        key={index}
+                        className='gallery__image'
+                        style={{ backgroundImage: `url(${imageUrl})`}}
+                    ></div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
